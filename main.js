2035241d8f81b128c37d902d948d98a8fb33fde8
Shane Mcgrath <smcgrath7777@gmail.com>
2019-07-06 11:28:44 -0700

    All cards can be flipped when clicked, and flipped back

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(initializeApp);
+
+function initializeApp(){
+  $(".card > div").addClass("lfzcard");
+  $(".card").on("click", handleCardClick)
+}
+
+function handleCardClick(event){
+  console.log(event);
+  if ($(event.target).hasClass("lfzcard")){
+    $(event.target).removeClass("lfzcard");
+  }else{
+    $(event.target).addClass("lfzcard");
+  }
+
+}
